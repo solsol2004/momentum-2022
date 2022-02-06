359ea56fa9c103e826f087e11e766f3aea60c40d
Stella <dpthf24@gmail.com>
2022-02-06 23:06:29 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  powerof: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(7, 10);
+calculator.minus(7, 10);
+calculator.divide(7, 10);
+calculator.powerof(7, 10);
