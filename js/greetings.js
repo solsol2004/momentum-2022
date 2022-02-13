a2ba441df8fc1bdec6c02a3d9ddf0cb18b73651b
Stella <dpthf24@gmail.com>
2022-02-14 00:01:20 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -14,7 +14,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Hello ${username} 🌱`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
